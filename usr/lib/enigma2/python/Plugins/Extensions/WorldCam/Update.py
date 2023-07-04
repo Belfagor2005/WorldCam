0fe7bf1d7842410dc07fd5bcef1408fa5f01ff9c
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2023-07-04 22:04:00 +0200

    Add files via upload

---
+++ usr/lib/enigma2/python/Plugins/Extensions/WorldCam/Update.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+PY3 = sys.version_info.major >= 3
+print("Update.py")
+
+
+def upd_done():
+    from twisted.web.client import downloadPage
+    print("In upd_done")
+    xfile = 'http://patbuweb.com/worldcam/worldcam.tar'
+    if PY3:
+        xfile = b"http://patbuweb.com/worldcam/worldcam.tar"
+        print("Update.py in PY3")
+    import requests
+    response = requests.head(xfile)
+    if response.status_code == 200:
+        # print(response.headers['content-length'])
+        fdest = "/tmp/worldcam.tar"
+        print("Code 200 upd_done xfile =", xfile)
+        downloadPage(xfile, fdest).addCallback(upd_last)
+    elif response.status_code == 404:
+        print("Error 404")
+    else:
+        return
+
+
+def upd_last(fplug):
+    import os
+    import time
+    time.sleep(5)
+    fdest = "/tmp/worldcam.tar"
+    if os.path.isfile(fdest) and os.stat(fdest).st_size > 10000:
+        cmd = "tar -xvf /tmp/worldcam.tar -C /"
+        print("cmd A =", cmd)
+        os.system(cmd)
+        os.remove('/tmp/worldcam.tar')
+    return
