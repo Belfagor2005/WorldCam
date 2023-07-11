4f9c2d4d18dc656f520e9c97578fd931b59904b4
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2023-07-11 20:21:48 +0200

    Up fix

--- usr/lib/enigma2/python/Plugins/Extensions/WorldCam/Update.py
+++ usr/lib/enigma2/python/Plugins/Extensions/WorldCam/Update.py
@@ -7,7 +7,7 @@ print("Update.py")
 
 
 def upd_done():
-    from os import popen
+    from os import popen, remove
     cmd01 = "wget http://patbuweb.com/worldcam/worldcam.tar -O /tmp/worldcam.tar ; tar -xvf /tmp/worldcam.tar -C /"
     cmd02 = "wget --no-check-certificate -U 'Enigma2 - worldcam Plugin' -c 'http://patbuweb.com/worldcam/worldcam.tar' -O '/tmp/worldcam.tar'; tar -xvf /tmp/worldcam.tar -C /"
     cmd22 = 'find /usr/bin -name "wget"'
@@ -19,6 +19,7 @@ def upd_done():
         popen(cmd02)
     except:
         popen(cmd01)
+    remove('/tmp/worldcam.tar')
     return
 
 '''
