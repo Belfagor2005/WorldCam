3da346ab10ee10e36d727b15a89f1a4e8fd33db2
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-06-21 00:58:25 +0200

    Revert "Fix Manifest Player on youtube"
    This reverts commit 73210792ee678d0ae21af85ba5c35ad6c426a354.

--- installer.sh
+++ installer.sh
@@ -3,8 +3,8 @@
 ## setup command=wget -q --no-check-certificate https://raw.githubusercontent.com/Belfagor2005/WorldCam/main/installer.sh -O - | /bin/sh
 
 ## Only This 2 lines to edit with new version ######
-version='5.1'
-changelog='\nFix Manifest on youtube Player\nAssert code separed fallback'
+version='5.0'
+changelog='\nRewrite all code source and add Youtube player'
 ##
 TMPPATH=/tmp/WorldCam-main
 FILEPATH=/tmp/main.tar.gz
